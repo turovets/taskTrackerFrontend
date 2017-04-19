726df4c58a79741f61d1fbf2feb9537454a9ef36
Varvara_Turavets <varvara_turavets@epam.com>
2017-04-19 19:42:36 +0300

    added navigation links

--- src/app/sideNav/components/SideNav.js
+++ src/app/sideNav/components/SideNav.js
@@ -1,29 +1,33 @@
 import React from 'react';
+import { Link } from 'react-router';
 
 const SideNav = ({headerTitle}) => (
   <div className="page-sidebar-wrapper">
     <div className="page-sidebar navbar-collapse collapse">
       <ul className="page-sidebar-menu   " data-keep-expanded="false" data-auto-scroll="true" data-slide-speed="200">
         <li className="nav-item start active">
-          <a href="#" className="nav-link nav-toggle">
+          <Link to={`/dashboard`}
+                className="nav-link nav-toggle">
             <i className="icon-home"></i>
             <span className="title">Dashboard</span>
             <span className="selected"></span>
-          </a>
+          </Link>
         </li>
         <li className="nav-item">
-          <a href="#" className="nav-link nav-toggle">
-            <i className="icon-layers"></i>
-            <span className="title">Projects</span>
-            <span className="selected"></span>
-          </a>
+            <Link to={`/projects`}
+                  className="nav-link nav-toggle">
+              <i className="icon-layers"></i>
+              <span className="title">Projects</span>
+              <span className="selected"></span>
+            </Link>
         </li>
         <li className="nav-item">
-          <a href="#" className="nav-link nav-toggle">
+          <Link to={`/account`}
+                className="nav-link nav-toggle">
             <i className="icon-user"></i>
             <span className="title">Account</span>
             <span className="selected"></span>
-          </a>
+          </Link>
         </li>
       </ul>
     </div>

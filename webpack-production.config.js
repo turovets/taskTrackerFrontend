ffd6be5a54d287003970523363cc1631848d88b1
Varvara_Turavets <varvara_turavets@epam.com>
2017-04-20 17:51:58 +0300

    added css loader

--- webpack-production.config.js
+++ webpack-production.config.js
@@ -41,6 +41,10 @@ const config = {
         loaders: ['babel-loader'], // react-hot is like browser sync and babel loads jsx and es6-7
         exclude: [nodeModulesPath],
       },
+	    {
+		    test: /\.css$/,
+		    loader:'style!css!'
+	    }
     ],
   },
 };

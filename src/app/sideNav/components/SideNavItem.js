b4e731f14851cd20ceca4f235d7808bda7d544e6
Varvara_Turavets <varvara_turavets@epam.com>
2017-04-20 17:49:37 +0300

    added link icon logic

--- src/app/sideNav/components/SideNavItem.js
+++ src/app/sideNav/components/SideNavItem.js
@@ -4,11 +4,27 @@ import { Link } from 'react-router';
 const SideNavItem = ({ title, linkUrl, isActive }) => {
 
 	const activeClassName = isActive() ? 'nav-item start active' : 'nav-item start ';
+	let iconClass;
+
+	switch (linkUrl) {
+		case 'dashboard':
+			iconClass = 'icon-home';
+			break;
+		case 'projects':
+			iconClass = 'icon-layers';
+			break;
+		case 'account':
+			iconClass = 'icon-user';
+			break;
+		default:
+			iconClass = 'icon-folder';
+	}
+
 	return (
 		<li className={activeClassName}>
 			<Link to={linkUrl}
 			      className="nav-link nav-toggle ">
-				<i className="icon-home"></i>
+				<i className={ iconClass }></i>
 				<span className="title">{title}</span>
 				<span className="selected"></span>
 			</Link>

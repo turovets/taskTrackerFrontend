de9e358dc44b1833bf486896f0698a6a7d175fc8
Varvara_Turavets <varvara_turavets@epam.com>
2017-05-16 11:26:03 +0300

    updated rootReducer logic to reset state to initial state when user do log out

--- src/app/rootReducer.js
+++ src/app/rootReducer.js
@@ -3,12 +3,21 @@ import tasks from './tasks/reducers/tasks'
 import projects from './projects/reducers/projects'
 import auth from './auth/reducers/index'
 import register from './register/reducers/index'
+import { LOGOUT_SUCCESS } from './auth/actions'
 
-const rootReducer = combineReducers({
+const appReducer = combineReducers({
   projects,
   tasks,
   auth,
   register
 });
+
+const rootReducer = (state, action) => {
+  if (action.type === LOGOUT_SUCCESS) {
+    state = undefined;
+  }
+
+  return appReducer(state, action)
+};
 
 export default rootReducer
